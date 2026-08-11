f4b8ed2ab5d8d9cd14fb98c09f74879f408c4d8b
siiko <siiko@tutamail.com>
2026-08-11 12:43:18 -0300

    I added comments that can help anyone who wants to modify the code

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ time.sleep(7)
 pyautogui.click(500, 250)
 
 time.sleep(5)
+
+# If u wanna put other songs, just change the url variable to the new song's search query. For example, if you want to search for "Bohemian Rhapsody", you can change the url variable like this:
+# url = "https://www.youtube.com/results?search_query=Bohemian+Rhapsody"
+# And if u wanna put the video on full screen, you can add the following line after the pyautogui.click() line:
+# pyautogui.press('f')
